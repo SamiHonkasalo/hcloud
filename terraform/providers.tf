8df05d18bf89d37f3c863666963203d2e08c233b
Sami Honkasalo <honkasalo.sami@gmail.com>
2023-12-01 12:17:03 +0200

    Add kubectl apply retry and disable local kubeconfig loading

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -19,8 +19,10 @@ provider "kubernetes" {
 }
 
 provider "kubectl" {
+  load_config_file = false
   host                   = local.kubeconfig_data.host
   cluster_ca_certificate = local.kubeconfig_data.cluster_ca_certificate
   client_certificate     = local.kubeconfig_data.client_certificate
   client_key             = local.kubeconfig_data.client_key
+  apply_retry_count = 2
 }
